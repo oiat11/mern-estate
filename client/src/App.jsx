793cc6a225dfb2478018f3f99f975c67442dc092
oiat11 <fu.qia@northeastern.edu>
2025-01-13 19:04:31 -0800

    created routes in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route, BrowserRouter} from 'react-router-dom'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
 
 export default function App() {
-  return (
-    <div className='text-red-500'>
-      App
-    </div>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/sign-in" element={<SignIn />} />
+    <Route path="/sign-out" element={<SignUp />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/profile" element={<Profile />} />
+  </Routes>
+  </BrowserRouter>
 }
