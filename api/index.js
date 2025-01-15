73e344588fe0d85d25068e0cee1ef450a833c6ee
oiat11 <fu.qia@northeastern.edu>
2025-01-15 14:59:46 -0800

    created a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -16,4 +17,4 @@ app.listen(3000, () => {
 });
 
 
-
+app.use('/api/user', userRoutes);
