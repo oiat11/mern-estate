b6ddb9ca07dd7f0b00b404d9ddde5123aaf32eef
oiat11 <fu.qia@northeastern.edu>
2025-01-19 18:55:19 -0800

    add redux persist in store.js and add the persist gate in main.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { store } from './redux/store.js'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
+  // use the Provider component to make the store available to the entire app
+  // use the PersistGate component to make the state persist across page reloads
   <Provider store={store}>
+    <PersistGate persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>,
 )
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import { persistReducer } from 'redux-persist'
+import { version } from 'mongoose'
+import storage from 'redux-persist/lib/storage'
+import persistStore from 'redux-persist/es/persistStore'
 
+const rootReducer = combineReducers({
+    user: userReducer,
+    })
+
+const persistConfig = {
+    key:'root',
+    storage,
+    version: 1,
+}
+
+const persistedReduer = persistReducer(persistConfig, rootReducer)
+
+// pass the persisted reducer to the configureStore function so that the state persists across page reloads
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReduer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   }),
 })
+
+export const persistor = persistStore(store);
