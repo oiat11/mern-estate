0714a5bbc3b53fac4ff2d5957e1864ab7a6f6c09
oiat11 <fu.qia@northeastern.edu>
2025-01-19 18:33:33 -0800

    use userslice in sign up page to manage the state

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,11 +1,14 @@
+// this file contains the user slice which is responsible for handling the user state in the redux store
 import { createSlice, current } from "@reduxjs/toolkit";
 
+// initial state of the user slice
 const initialState = {
     currentUser: null,
     error: null,
     loading: false,
 }
 
+// create a user slice with the initial state and reducers
 const userSlice = createSlice({
     name:"user",
     initialState,
