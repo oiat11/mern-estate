96270fe3ccdf34e4583e97e3a79c1c9091bf410e
oiat11 <fu.qia@northeastern.edu>
2025-01-19 17:08:43 -0800

    created sign in api route by using JWT to authorize

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import {errorHandler} from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 // signup controller to create a new user in the database
 // use bcryptjs to hash the password before saving it to the database
@@ -15,3 +17,28 @@ export const signup = async(req, res, next) => {
     }
 
 };
+
+export const signin = async(req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        // check if the email is correct in the database
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, "User not found"));
+
+        // check if the password is correct with bcryptjs
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+
+        // create a token with jwt and send it to the client
+        // destruct the password from the user object so it is not sent to the client
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        const { password: pass, ...rest } = validUser._doc;
+
+        // send the token and the user data to the client
+        res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { signup } from '../controller/auth.controller.js';
+import { signup, signin } from '../controller/auth.controller.js';
 
 const router = express.Router();
 
 // signup route to create a new user
 router.post("/signup", signup);
+
+// signin route to sign in a user
+router.post("/signin", signin);
 
 export default router;
