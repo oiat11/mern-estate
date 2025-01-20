4efa9f0b6319fbf2c2489959d5cb7f59330979ee
oiat11 <fu.qia@northeastern.edu>
2025-01-20 14:51:04 -0800

    add OAuth in sign in and sign up page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 
 export default function SignIn() {
@@ -71,7 +72,9 @@ export default function SignIn() {
         />
         <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-85 disabled:opacity-70">
           {loading ? "Loading..." : "Sign in"}
+          
         </button>
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Don't have an account?</p>
