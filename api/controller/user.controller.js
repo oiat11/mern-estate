c0885a056a1334b3d166a361969a9bba4d3c6b9e
oiat11 <fu.qia@northeastern.edu>
2025-01-31 15:27:29 -0800

    create getUserListings api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Listing from "../models/listing.model.js";
 import bcrypt from "bcryptjs";
 import {errorHandler} from "../utils/error.js";
 
@@ -46,3 +47,16 @@ export const deleteUser = async (req, res, next) => {
         next(err);
     }
 }
+
+export const getUserListings = async (req, res, next) => {
+    if (req.user.id === req.params.id) {
+      try {
+        const listings = await Listing.find({ userRef: req.params.id });
+        res.status(200).json(listings);
+      } catch (error) {
+        next(error);
+      }
+    } else {
+      return next(errorHandler(401, 'You can only view your own listings!'));
+    }
+  };
