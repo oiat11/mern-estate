71429d22786271a7ed2461895426f99500cfe493
oiat11 <fu.qia@northeastern.edu>
2025-01-31 17:18:06 -0800

    complete user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -11,6 +11,7 @@ import {
 import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserFailure, deleteUserSuccess, signOutUserStart, signOutUserSuccess, signOutUserFailure } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 import {Link} from 'react-router-dom';
+import { set } from "mongoose";
 
 export default function Profile() {
   // the fileRef is used to reference the file input element to open the file dialog when the user clicks on the profile picture, so that we can hide the input element
@@ -34,6 +35,8 @@ export default function Profile() {
   const [updateSuccess, setUpdateSuccess] = useState(false);
   
   const dispatch = useDispatch();
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 
   useEffect(() => {
     if (file) {
@@ -142,6 +145,22 @@ const handleSignOut = async () => {
   }
 }
 
+const handleShowListings = async () => {
+  try{
+    setShowListingsError(false);
+    const res = await fetch(`/api/user/listings/${currentUser._id}`);
+    const data = await res.json();
+    if (data.success === false) {
+      setShowListingsError(true);
+      return;
+    }
+    setUserListings(data);
+    setShowListingsError(false);
+  } catch {
+    setShowListingsError(true);
+  }
+}
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -210,6 +229,26 @@ const handleSignOut = async () => {
       {updateSuccess && (
         <p className="text-green-700 mt-5">Profile updated successfully</p>
       )}
+      <button onClick={handleShowListings} className="text-green-700 w-full">Show Listings</button>
+      <p className="text-red-700 mt-5">{showListingsError ?'Error show listings' :'' }</p>
+      {userListings && userListings.length > 0 &&
+      <div className="flex flex-col gap-4">
+        <h1 className="text-center mt-7 text-2xl">Your Listings</h1>
+      {userListings.map((listing) => (
+        <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+          <Link to={`/listing/${listing._id}` } className="flex items-center gap-4 flex-1">
+          <img src={listing.imageUrls[0]} alt='listing cover' className="h-16 w-16 object-contain"></img>
+          <p className="text-slate-700 font-semibold hover:underline truncate">{listing.title}</p>
+          </Link>
+   
+          <div className="flex flex-col items-center">
+            <button className="text-red-700 uppercase">Delete</button>
+            <button className="text-green-700 uppercase">Edit</button>
+            </div>
+        </div>
+      ))}
+      </div>
+      }
     </div>
   );
 }
