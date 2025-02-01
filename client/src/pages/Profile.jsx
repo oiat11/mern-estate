ef482c6fb23e5dbe1f61638bfc806de070197855
oiat11 <fu.qia@northeastern.edu>
2025-01-31 17:49:35 -0800

    complete user listing delete functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -161,6 +161,22 @@ const handleShowListings = async () => {
   }
 }
 
+const handlelistingDelete = async (listingid) => {
+  try{
+    const res = await fetch(`/api/listing/delete/${listingid}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if (data.success === false) {
+      console.log(data.message);
+      return;
+    }
+    setUserListings((prevListings) => prevListings.filter((listing) => listing._id !== listingid));
+  } catch {
+    console.log(error.message);
+  }
+}
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -242,7 +258,7 @@ const handleShowListings = async () => {
           </Link>
    
           <div className="flex flex-col items-center">
-            <button className="text-red-700 uppercase">Delete</button>
+            <button onClick={()=>handlelistingDelete(listing._id)} className="text-red-700 uppercase">Delete</button>
             <button className="text-green-700 uppercase">Edit</button>
             </div>
         </div>
