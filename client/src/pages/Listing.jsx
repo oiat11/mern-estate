7ea9fa58542b2815d6b11f299ffb3e0754c019ce
oiat11 <fu.qia@northeastern.edu>
2025-02-02 18:06:31 -0800

    add the UI of listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
-import {FaShare} from 'react-icons/fa';
+import {FaBath, FaBed, FaChair, FaMapMarkedAlt, FaMapMarkerAlt, FaParking, FaShare} from 'react-icons/fa';
 import { set } from "mongoose";
 
 export default function Listing() {
@@ -69,6 +69,42 @@ export default function Listing() {
             <FaShare className="text-slate-500" onClick={handleCopy}/>
           </div>
           {copied && <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">Link copied!</p>}
+          <div className="flex flex-col max-w-4xl mx-auto p-3 my-7 gap-6">
+            <p className="text-2xl font-semibold">
+                {listing.title} - ${' '} {
+                    listing.offer ? listing.discountPrice.toLocaleString('en-US') : listing.regularPrice.toLocaleString('en-US')
+                }
+                {listing.type ==='rent' ? ' / month' : ''}
+            </p>
+            <p className="flex items-center mt-6 gap-2 text-slate-600 my-2 text-sm">
+                <FaMapMarkerAlt className="text-green-700" />
+                {listing.address}
+            </p>
+            <div className="flex gap-4">
+                <p className="bg-red-900 w-full max-w-[200px] text-white text-center p-2 rounded-md">
+                    {listing.type === 'rent' ? 'For Rent' : 'For Sale'}
+                </p>
+                {listing.offer && (
+                    <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-2 rounded-md"> ${(+listing.regularPrice - +listing.discountPrice).toLocaleString()} discount</p>
+                )}
+            </div>
+            <p className="text-slate-800"><span className="font-semibold text-black">Description - </span>{listing.description}</p>
+            <ul className="text-green-800 font-semibold text-sm flex flex-wrap items-center gap-4 sm:gap-6">
+                <li className="flex items-center gap-1 whitespace-nowrap text-green-800 font-semibold text-sm">
+                    <FaBed className="text-lg" /> {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : `${listing.bedrooms} Bedroom`}
+                </li>
+                <li className="flex items-center gap-1 whitespace-nowrap text-green-800 font-semibold text-sm">
+                    <FaBath className="text-lg" /> {listing.bedroom > 1 ? `${listing.bathrooms} Bathrooms` : `${listing.bathrooms} Bathroom`}
+                </li>
+                <li className="flex items-center gap-1 whitespace-nowrap text-green-800 font-semibold text-sm">
+                    <FaParking className="text-lg" /> {listing.parking ? 'Parking Available' : 'No Parking'}
+                </li>
+                <li className="flex items-center gap-1 whitespace-nowrap text-green-800 font-semibold text-sm">
+                    <FaChair className="text-lg" /> {listing.furnished ? 'Furnished' : 'Unfurnished'}
+                </li>
+            </ul>
+          </div>
+        
         </div>
       )}
     </main>
