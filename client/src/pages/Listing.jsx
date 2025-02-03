a1b364562c2b3f849ee3bb45cdf03cb9baf0ad09
oiat11 <fu.qia@northeastern.edu>
2025-02-02 17:18:52 -0800

    add an share icon in the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -4,12 +4,15 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import {FaShare} from 'react-icons/fa';
+import { set } from "mongoose";
 
 export default function Listing() {
   SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [copied, setCopied] = useState(false);
 
   const params = useParams();
   useEffect(() => {
@@ -33,6 +36,14 @@ export default function Listing() {
     fetchListing();
   }, [params.id]);
 
+  const handleCopy = (event) => {
+    event.stopPropagation();
+    navigator.clipboard.writeText(window.location.href);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
+  };
+
+
   return (
     <main>
       {loading && <p className="text-center my-7 text-2xl">Loading...</p>}
@@ -54,6 +65,10 @@ export default function Listing() {
               </SwiperSlide>
             ))}
           </Swiper>
+          <div className="fixed top-[13%] right-[5%] z-10 border rounded-full w-12 h-12 flex justify-center items-center bg-slate-100 cursor-pointer">
+            <FaShare className="text-slate-500" onClick={handleCopy}/>
+          </div>
+          {copied && <p className="fixed top-[23%] right-[5%] z-10 rounded-md bg-slate-100 p-2">Link copied!</p>}
         </div>
       )}
     </main>
