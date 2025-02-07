003f500fa6be3da02f67a65f7040aff5dda53156
oiat11 <fu.qia@northeastern.edu>
2025-02-06 16:10:59 -0800

    complete home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
 
   return (
     <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+      <div className="flex justify-between items-center max-w-7xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex-wrap">
             <span className="text-slate-500">Estate</span>
