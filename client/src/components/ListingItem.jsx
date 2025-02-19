774f7deb28b4e9b471577a9cea10989c62e17e6b
oiat11 <fu.qia@northeastern.edu>
2025-02-19 15:45:12 -0800

    make the entire listingItem card clickable

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -3,45 +3,47 @@ import { MdLocationOn } from "react-icons/md";
 
 export default function ListingItem({ listing }) {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[700px] flex">
-      <Link to={`/listing/${listing._id}`} className="flex-shrink-0">
-        <img
-          src={listing.imageUrls[0]}
-          alt="listing cover"
-          className="h-[200px] w-[300px] object-cover hover:scale-105 transition-scale duration-300"
-        />
-      </Link>
-      <div className="p-4 flex flex-col gap-2 w-full">
-        <p className="truncate text-lg font-semibold text-slate-700">
-          {listing.title}
-        </p>
-        <div className="flex items-center gap-2">
-          <MdLocationOn className="h-4 w-4 text-green-700" />
-          <p className="text-sm text-gray-600 truncate">{listing.address}</p>
+    <Link to={`/listing/${listing._id}`} className="block w-full sm:w-[700px]">
+      <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full flex">
+        <div className="flex-shrink-0">
+          <img
+            src={listing.imageUrls[0]}
+            alt="listing cover"
+            className="h-[200px] w-[300px] object-cover hover:scale-105 transition-scale duration-300"
+          />
         </div>
-        <p className="text-sm text-gray-600 line-clamp-2">
-          {listing.description}
-        </p>
-        <p className="text-slate-500 mt-2 font-semibold flex items-center">
-          $
-          {listing.offer
-            ? listing.discountPrice.toLocaleString("en-US")
-            : listing.regularPrice.toLocaleString("en-US")}
-          {listing.type === "rent" && "/ month"}
-        </p>
-        <div className="flex items-center gap-4 text-slate-700">
-          <div className="font-bold text-xs">
-            {listing.bedrooms > 1
-              ? `${listing.bedrooms} bedrooms`
-              : `${listing.bedrooms} bedroom`}
+        <div className="p-4 flex flex-col gap-2 w-full">
+          <p className="truncate text-lg font-semibold text-slate-700">
+            {listing.title}
+          </p>
+          <div className="flex items-center gap-2">
+            <MdLocationOn className="h-4 w-4 text-green-700" />
+            <p className="text-sm text-gray-600 truncate">{listing.address}</p>
           </div>
-          <div className="font-bold text-xs">
-            {listing.bathrooms > 1
-              ? `${listing.bathrooms} bathrooms`
-              : `${listing.bathrooms} bathroom`}
+          <p className="text-sm text-gray-600 line-clamp-2">
+            {listing.description}
+          </p>
+          <p className="text-slate-500 mt-2 font-semibold flex items-center">
+            $
+            {listing.offer
+              ? listing.discountPrice.toLocaleString("en-US")
+              : listing.regularPrice.toLocaleString("en-US")}
+            {listing.type === "rent" && "/ month"}
+          </p>
+          <div className="flex items-center gap-4 text-slate-700">
+            <div className="font-bold text-xs">
+              {listing.bedrooms > 1
+                ? `${listing.bedrooms} bedrooms`
+                : `${listing.bedrooms} bedroom`}
+            </div>
+            <div className="font-bold text-xs">
+              {listing.bathrooms > 1
+                ? `${listing.bathrooms} bathrooms`
+                : `${listing.bathrooms} bathroom`}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
